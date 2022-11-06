4bf6289280ed8ac803e70179d72a741c3e6cdbcc
yen031301 <cyen1@artic.edu>
2022-11-06 17:11:44 -0600

    main

--- scripts/header-footer.js
+++ scripts/header-footer.js
@@ -14,6 +14,6 @@ $('#header-container').load('/components/header.html', function(){
 
 // FOOTER
 
-const footer = `<footer>&copy; ${new Date().getFullYear()} Portfolio Example</footer>`
+const footer = `<footer>&copy; ${new Date().getFullYear()} Avril's Portfolio | avrilyen01@gmail.com </footer>`
 
 $('body').append(footer)
